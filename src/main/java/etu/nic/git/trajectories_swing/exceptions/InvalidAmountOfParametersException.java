1104321c1f25891c3621c946e278be832c52c0cd
Ткачев Андрей Дмитриевич <21000102@edu.spbgasu.ru>
2024-09-10 11:51:23 +0300

    commented exceptions

--- src/main/java/etu/nic/git/trajectories_swing/exceptions/InvalidAmountOfParametersException.java
+++ src/main/java/etu/nic/git/trajectories_swing/exceptions/InvalidAmountOfParametersException.java
@@ -1,5 +1,8 @@
 package etu.nic.git.trajectories_swing.exceptions;
 
+/**
+ * Исключение, выбрасываемое в случае, если в строке файла траекторной информации неверное количество параметров
+ */
 public class InvalidAmountOfParametersException extends Exception {
     public InvalidAmountOfParametersException() {
         super();
--- src/main/java/etu/nic/git/trajectories_swing/exceptions/InvalidFileFormatException.java
+++ src/main/java/etu/nic/git/trajectories_swing/exceptions/InvalidFileFormatException.java
@@ -1,5 +1,8 @@
 package etu.nic.git.trajectories_swing.exceptions;
 
+/**
+ * Исключение, выбрасываемое в случае, файл траекторной информации имеет неверный формат
+ */
 public class InvalidFileFormatException extends Exception{
     public InvalidFileFormatException() {
         super();
